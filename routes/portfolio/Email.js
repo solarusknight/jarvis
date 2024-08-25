26cce546bb546955bccc4220244dadd36c7e2e52
solarusknight <wolfwarriersolaris@gmail.com>
2024-08-25 20:55:51 +0530

    Jarvis debugging

--- routes/portfolio/Email.js
+++ routes/portfolio/Email.js
@@ -3,4 +3,5 @@ const router = express.Router();
 const {sendEmail} = require('../../controllers/portfolio/Email');
 const {validateFormData,validateEmail} = require('../../middlewares/Email');
 router.post("/sendEmail",validateFormData,validateEmail,sendEmail);
+router.get("/sendEmail",(req,res)=>{res.send("Hi")});
 module.exports = router;
